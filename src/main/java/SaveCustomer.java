89692903be195009af516f52628772572c9c993e
张强 <1378082472@qq.com>
2019-01-04 16:06:24 +0800

    hello_maven

---
+++ src/main/java/SaveCustomer.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 张强
+ * created on 2018/12/28  18:47:22
+ * description
+ */
+public class SaveCustomer extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        String name = req.getParameter("name");
+        System.out.println("name="+name);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form method="post" action="SaveCustomer">
+    姓名：<input type="text" name="name"><br/>
+    <input type="submit" value="提交">
+</form>
+</body>
+</html>
